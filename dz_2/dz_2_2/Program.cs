97c764bbe603b259e3843fa92b9492bb033b91a2
cirill98 <100434317+cirill98@users.noreply.github.com>
2022-08-10 16:13:06 +0500

    dz 2

---
+++ dz_2/dz_2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}    
+while (a > 999)
+{
+    a = a / 10;
+    if (a < 1000)
+    {
+    Console.WriteLine(a % 10);   
+    }
+}
+
